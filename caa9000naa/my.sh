e96f453e157647d129adcf71113c6521ce167141
AtoosaNasiri-Seneca <atoosa.nasiri@senecapolytechnic.ca>
2024-01-30 23:11:50 -0500

    updates cp4

---
+++ caa9000naa/my.sh
@@ -0,0 +1,11 @@
+USER_NAME="atoosanasiri"
+ADMIN_PW="@atoosanasiri123"
+
+RG_NAME="Student-RG-1202253"     # your student group
+LOCATION="canadaeast"    # your location
+ID="99"          #unique ID assigned to you
+
+Student_vnet_name="Student-1202253-vnet"
+Student_vnet_address="10.25.173.0/24"
+Client_Subnet_name="Virtual-Desktop-Client"
+Client_Subnet_address="10.25.173.0/24"
