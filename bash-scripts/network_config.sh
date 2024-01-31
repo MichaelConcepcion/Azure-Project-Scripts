8cd92288a4c47bdc54a705eaf5f544f77aeb585f
Atoosa Nasiri <98177145+AtoosaNasiri-Seneca@users.noreply.github.com>
2024-01-31 18:28:19 -0500

    Update network_config.sh

--- bash-scripts/network_config.sh
+++ bash-scripts/network_config.sh
@@ -24,14 +24,6 @@ Server_vnet_name="Server-$ID"
 Router_vnet_address="192.168.$ID.0/24"
 Server_vnet_address="172.17.$ID.0/24"
 
-RG_NAME="Student-RG-1202253"     # your student group
-LOCATION="canadaeast"    # your location
-ID="99"          #unique ID assigned to you
-
-Student_vnet_name="Student-1202253-vnet"
-Student_vnet_address="10.25.173.0/24"
-Client_Subnet_name="Virtual-Desktop-Client"
-Client_Subnet_address="10.25.173.0/24"
 # ---------------------------------------------------
 # Networking - Virtual Networks - Subnets 
 declare -a Client_Subnet_list=("$Client_Subnet_name" "$Client_Subnet_Address")
